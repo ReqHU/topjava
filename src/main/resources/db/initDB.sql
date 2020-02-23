38056416a183afde856a3941d574381f55806e97
ReqHU <jenia010696@gmail.com>
2020-02-24 01:57:01 +0300

    HW3

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
@@ -19,7 +20,18 @@ CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 CREATE TABLE user_roles
 (
   user_id INTEGER NOT NULL,
-  role    VARCHAR,
+  role VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+    id INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    user_id INTEGER NOT NULL,
+    dateTime TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    description VARCHAR NOT NULL,
+    calories INTEGER NOT NULL,
+    CONSTRAINT meals_idx UNIQUE (id, user_id),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -9,3 +10,13 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, dateTime, description, calories) VALUES
+  (100001, '2020-01-30T10:00', 'Завтрак', 500),
+  (100001, '2020-01-30T13:00', 'Обед', 1000),
+  (100001, '2020-01-30T20:00', 'Ужин', 500),
+  (100001, '2020-01-31T00:00', 'Еда на граничное значение', 100),
+  (100001, '2020-01-31T10:00', 'Завтрак', 1000),
+  (100001, '2020-01-31T13:00', 'Обед', 500),
+  (100001, '2020-01-31T20:00', 'Ужин', 410);
+
