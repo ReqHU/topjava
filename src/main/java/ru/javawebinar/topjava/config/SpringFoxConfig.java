12e0f506b8d1e8d25a443828b1683040c92f2728
Evgeny Nagorny <jenia010696@gmail.com>
2020-11-25 19:10:53 +0300

    Added Swagger Security

--- src/main/java/ru/javawebinar/topjava/config/SpringFoxConfig.java
+++ src/main/java/ru/javawebinar/topjava/config/SpringFoxConfig.java
@@ -8,9 +8,17 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.BasicAuth;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -21,9 +29,28 @@ public class SpringFoxConfig extends WebMvcConfigurerAdapter {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .securitySchemes(Collections.singletonList(securityScheme()))
+                .securityContexts(Collections.singletonList(securityContext()))
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("ru.javawebinar.topjava.web"))
                 .paths(PathSelectors.any())
+                .build();
+    }
+
+    private SecurityScheme securityScheme() {
+        return new BasicAuth("basicAuth");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(Arrays.asList(basicAuthReference()))
+                .build();
+    }
+
+    private SecurityReference basicAuthReference() {
+        return SecurityReference.builder()
+                .reference("basicAuth")
+                .scopes(new AuthorizationScope[0])
                 .build();
     }
 
